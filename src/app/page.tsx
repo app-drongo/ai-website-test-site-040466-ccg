f81553e2dd4ff9466b122d994bda2ede7b155929
AI Website Generator <generator@yourplatform.com>
2025-11-02 09:50:45 +0000

    Initial website generation

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from '@/components/sections/home/Hero'
+import Pricing from '@/components/sections/home/Pricing'
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <Pricing />
+    </>
+  )
+}
